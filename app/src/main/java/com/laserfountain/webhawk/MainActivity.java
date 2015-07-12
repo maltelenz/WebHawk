b79e25268697912860413ecaf3b74f866c397fe7
Malte Lenz <malte.lenz@gmail.com>
2015-07-12 16:48:21 +0200

    Set up some basic layouts and colors.

--- app/src/main/java/com/laserfountain/webhawk/MainActivity.java
+++ app/src/main/java/com/laserfountain/webhawk/MainActivity.java
@@ -2,11 +2,12 @@ package com.laserfountain.webhawk;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
